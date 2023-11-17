25e079feced687bc94c2ca24761a7840943fb853
andresjaramillo7 <a01029079@tec.mx>
2023-11-17 09:44:02 -0600

    src, db, routes, views, controllers

---
+++ database/db.sql
@@ -0,0 +1,19 @@
+-- creating the db
+CREATE DATABASE crud_test;
+
+-- using the db
+use crud_test;
+
+-- creating a table
+CREATE TABLE customer(
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    phone VARCHAR(15)
+);
+
+-- to show tables
+SHOW TABLES;
+
+-- to describe the table
+describe customer;
