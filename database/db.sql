88aa96803cff09b12ca3e3d6a8866174e30bd534
andresjaramillo7 <a01029079@tec.mx>
2023-11-25 09:39:07 -0600

    Updated DataBase frin address to email

--- database/db.sql
+++ database/db.sql
@@ -8,7 +8,7 @@ use crud_test;
 CREATE TABLE customer(
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    address VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     phone VARCHAR(15)
 );
 
